cf50654799dfbba4b04f9bbe46c68cfc2e0c4b22
Sean Harrington <sean.harrington@martinagency.com>
2019-08-06 21:43:29 -0400

    Add DO Tween, question coroutine

---
+++ Assets/TweenManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+//http://dotween.demigiant.com/getstarted.php
+// http://dotween.demigiant.com/documentation.php#shortcuts
+
+public class TweenManager : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+}
--- Assets/UI/RecordUIController.cs
+++ Assets/UI/RecordUIController.cs
@@ -2,15 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
 
 // see https://github.com/yasirkula/UnityNativeCamera
+
+// http://dotween.demigiant.com/getstarted.php
+// http://dotween.demigiant.com/documentation.php#shortcuts
 
 public class RecordUIController : MonoBehaviour
 {
   AppManager app;
   bool hasCam;
   private bool isRecording = false;
-  public float TimeToRecord; 
+  public float TimeToRecord;
+
+  public TMPro.TextMeshProUGUI questionText;
+  public string[] questions;
+  public float questionSeconds;
+
+  int currentQuestion = 0;
+
 
   void Start()
   {
@@ -21,58 +32,64 @@ public class RecordUIController : MonoBehaviour
     //app.log("DeviceHasCamera is " + hasCam);
 
 
-            // Set the time you are allowing the user to record gameplay
-        ReplayKitUnity.AllowedTimeToRecord = TimeToRecord; 
+    // Set the time you are allowing the user to record gameplay
+    ReplayKitUnity.AllowedTimeToRecord = TimeToRecord;
 
-        // Tells ReplayKit to use a default interface that is excluded in playback         
-        ReplayKitUnity.ShowDefaultButtonUI();
+    // Tells ReplayKit to use a default interface that is excluded in playback         
+    ReplayKitUnity.ShowDefaultButtonUI();
 
-        // Subscribe to the ReplayKit callbacks 
-        if (ReplayKitUnity.IsScreenRecorderAvailable) {    
-            ReplayKitUnity.Instance.onStopScreenCaptureWithFile += OnStopCallback;
-            ReplayKitUnity.Instance.onStartScreenCapture += OnStartRecording;                
-        }
+    // Subscribe to the ReplayKit callbacks 
+    if (ReplayKitUnity.IsScreenRecorderAvailable)
+    {
+      ReplayKitUnity.Instance.onStopScreenCaptureWithFile += OnStopCallback;
+      ReplayKitUnity.Instance.onStartScreenCapture += OnStartRecording;
+    }
 
 
   }
 
-      // Call back that is triggered from iOS native 
-    public void OnStartRecording() {
-        if (!isRecording) {
-            isRecording = true; 
-        }
+  // Call back that is triggered from iOS native 
+  public void OnStartRecording()
+  {
+    if (!isRecording)
+    {
+      isRecording = true;
     }
 
+    StartCoroutine("AskQuestions");
+  }
 
-    // You will recieve the file path to the recorded gameplay session here 
-    public void OnStopCallback(string files) {
-        isRecording = false;
 
-        // Play the recorded video 
-        //recordCallback(file);
-        app.log("FilePath is totally rthis  " + files);
+  // You will recieve the file path to the recorded gameplay session here 
+  public void OnStopCallback(string files)
+  {
+    isRecording = false;
 
-        app.VideoURL = files.Remove(0,7);
-        GoToShare();
-    }
+    // Play the recorded video 
+    //recordCallback(file);
+    app.log("FilePath is totally rthis  " + files);
+
+    app.VideoURL = files.Remove(0, 7);
+    GoToShare();
+  }
 
   void Update()
+  {
+    // Handle screen touches.
+    if (Input.touchCount > 0)
     {
-        // Handle screen touches.
-        if (Input.touchCount > 0)
-        {
-            Touch touch = Input.GetTouch(0);
-            if (touch.phase == TouchPhase.Began)
-            {
-                ReplayKitUnity.StartRecording();
-            }
+      Touch touch = Input.GetTouch(0);
+      if (touch.phase == TouchPhase.Began)
+      {
+        ReplayKitUnity.StartRecording();
+      }
 
-            if (touch.phase == TouchPhase.Ended)
-            {
-                ReplayKitUnity.StopRecording();
-            }
-        }
+      if (touch.phase == TouchPhase.Ended)
+      {
+        ReplayKitUnity.StopRecording();
+      }
     }
+  }
 
   public void handleRecordButtonClick()
   {
@@ -103,8 +120,22 @@ public class RecordUIController : MonoBehaviour
     }
   }
 
-  void GoToShare() 
+  void GoToShare()
   {
-        SceneManager.LoadScene("3_ReviewShare");
+    SceneManager.LoadScene("3_ReviewShare");
+  }
+
+  IEnumerator AskQuestions()
+  {
+    while (currentQuestion < questions.Length)
+    {
+      questionText.text = questions[currentQuestion];
+
+      questionText.transform.DOPunchScale(Vector3.one * 2, 2.0f, 10, 1);
+
+      yield return new WaitForSeconds(questionSeconds);
+
+      currentQuestion++;
+    }
   }
 }
